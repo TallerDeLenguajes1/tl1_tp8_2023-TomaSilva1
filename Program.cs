a8c79c2e40bb03d10b91342517bd3392d8a9dcbc
TomaSilva1 <tomasilva302@gmail.com>
2023-06-28 17:23:21 -0300

    Creacion de las listas y el interfaz

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using tareas;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random random = new Random();
+
+int cantPend = random.Next(1, 3);
+
+List<ToDo> tareaPendiente = new List<ToDo>();
+List<ToDo> tareaRealizada = new List<ToDo>();
+
+for (int i = 0; i < cantPend; i++)
+{
+    Console.WriteLine("--------Cargar tareas--------");
+    Console.WriteLine("Ingrese la descripcion de la tarea: ");
+    
+}
---
+++ ToDo.cs
@@ -0,0 +1,16 @@
+namespace tareas
+{
+    public class ToDo
+    {
+        
+        private int tareaId;
+        public int TareaId { get => tareaId; set => tareaId = value; }
+
+        private string descripcion;
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        private int duracion;
+        public int Duracion { get => duracion; set => duracion = value; }
+
+    }
+}
