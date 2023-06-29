623d3352e5c85d25a9cd99a31f2b002343ca1430
TomaSilva1 <tomasilva302@gmail.com>
2023-06-28 23:36:49 -0300

    fin tp8

--- Program.cs
+++ Program.cs
@@ -18,3 +18,18 @@ using System.IO;
             Console.WriteLine("La carpeta que ingreso no existe.");
         }
     
+        string[] contenido = Directory.GetFiles(ruta);
+
+        using (StreamWriter cargar = new StreamWriter("index.csv"))
+        {
+            cargar.WriteLine("Indice, Nombre, Extension");
+        
+
+            for (int i = 0; i < contenido.Length; i++)
+            {
+                string nombre = Path.GetFileNameWithoutExtension(contenido[i]);
+                string extension = Path.GetExtension(contenido[i]);
+                int cont = i+1;
+                cargar.WriteLine(cont+","+nombre+","+extension);
+            }
+        }
